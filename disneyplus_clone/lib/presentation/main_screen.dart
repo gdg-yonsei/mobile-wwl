7d0e3294a333c71b537ac54f7936b84a64b12efe
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-09-24 20:55:00 +0900

    [Feat] 프랜차이즈 버튼 리스트 구현

--- disneyplus_clone/lib/presentation/main_screen.dart
+++ disneyplus_clone/lib/presentation/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:disneyplus_clone/presentation/BottomNavigationBar.dart';
+import 'package:disneyplus_clone/presentation/FranchiseButton.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -15,8 +16,32 @@ class MainScreen extends StatelessWidget {
         backgroundColor: const Color(0xff39416f),
       ),
       body: const SafeArea(
-        child: Center(
-          child: Text('Hello World'),
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.all(20), child: Column(
+              // @TODO : Flexible 아닌 방법 혹은 Flexible 쓰면서 더 좋은 방법 찾기
+              children: [
+                Row(
+                  children: [
+                    Flexible(child: FranchiseButton(title: "star wars", image: 'assets/image/starwars_logo.png')),
+                    SizedBox(width: 10,),
+                    Flexible(child: FranchiseButton(title: "star channel", image: 'assets/image/star_logo.png')),
+                    SizedBox(width: 10,),
+                    Flexible(child: FranchiseButton(title: "pixar", image: "assets/image/pixar_logo.png")),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Flexible(child: FranchiseButton(title: "national geographic", image: "assets/image/ng_logo.png")),
+                    SizedBox(width: 10,),
+                    Flexible(child: FranchiseButton(title: "marvel", image: 'assets/image/marvel_logo.png')),
+                    SizedBox(width: 10,),
+                    Flexible(child: FranchiseButton(title: "disney", image: 'assets/image/disney_white_logo.png'))
+                  ],
+                ),
+              ],
+            ),)
+          ],
         )
       ),
       backgroundColor: const Color(0xff39416f),
