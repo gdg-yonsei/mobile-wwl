f8a13a7e9eb3e3b20bc9a02033cd58230b54a347
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-10-02 12:59:51 +0900

    [Feat] 백그라운드 이미지 및 로고 변경

--- disneyplus_clone/lib/presentation/main_screen.dart
+++ disneyplus_clone/lib/presentation/main_screen.dart
@@ -10,53 +10,62 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Image.network(
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Disney%2B_logo.svg/2560px-Disney%2B_logo.svg.png',
-          height: 40,
-        ),
-        backgroundColor: const Color(0xff39416f),
-        centerTitle: true,
-      ),
-      body: const SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              MainCarousel(),
-              Padding(padding: EdgeInsets.all(20), child: Column(
-                // @TODO : Flexible 아닌 방법 혹은 Flexible 쓰면서 더 좋은 방법 찾기
-                children: [
-                  Row(
-                    children: [
-                      Expanded(child: FranchiseButton(title: "star wars", image: 'assets/image/starwars_logo.png')),
-                      SizedBox(width: 10,),
-                      Expanded(child: FranchiseButton(title: "star channel", image: 'assets/image/star_logo.png')),
-                      SizedBox(width: 10,),
-                      Expanded(child: FranchiseButton(title: "pixar", image: "assets/image/pixar_logo.png")),
-                    ],
-                  ),
-                  SizedBox(height: 8,),
-                  Row(
-                    children: [
-                      Expanded(child: FranchiseButton(title: "national geographic", image: "assets/image/ng_logo.png")),
-                      SizedBox(width: 10,),
-                      Expanded(child: FranchiseButton(title: "marvel", image: 'assets/image/marvel_logo.png')),
-                      SizedBox(width: 10,),
-                      Expanded(child: FranchiseButton(title: "disney", image: 'assets/image/disney_white_logo.png'))
-                    ],
-                  ),
-                ],
-              ),),
-              ContentsListSection(title: "픽사 오리지널", images: pixarContents),
-              ContentsListSection(title: "스타워즈", images: starwarsContents),
-              ContentsListSection(title: "한국 드라마", images: koreanContents),
-            ],
-          ),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/image/background.png'),
+          fit: BoxFit.cover,
         )
       ),
-      backgroundColor: const Color(0xff39416f),
-      bottomNavigationBar: const CustomBottomNavigationBar(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Image.asset(
+            'assets/image/logo.png',
+            height: 40,
+            // colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn)
+          ) ,
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
+        ),
+        body: const SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                MainCarousel(),
+                Padding(padding: EdgeInsets.all(20), child: Column(
+                  // @TODO : Flexible 아닌 방법 혹은 Flexible 쓰면서 더 좋은 방법 찾기
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(child: FranchiseButton(title: "star wars", image: 'assets/image/starwars_logo.png')),
+                        SizedBox(width: 10,),
+                        Expanded(child: FranchiseButton(title: "star channel", image: 'assets/image/star_logo.png')),
+                        SizedBox(width: 10,),
+                        Expanded(child: FranchiseButton(title: "pixar", image: "assets/image/pixar_logo.png")),
+                      ],
+                    ),
+                    SizedBox(height: 8,),
+                    Row(
+                      children: [
+                        Expanded(child: FranchiseButton(title: "national geographic", image: "assets/image/ng_logo.png")),
+                        SizedBox(width: 10,),
+                        Expanded(child: FranchiseButton(title: "marvel", image: 'assets/image/marvel_logo.png')),
+                        SizedBox(width: 10,),
+                        Expanded(child: FranchiseButton(title: "disney", image: 'assets/image/disney_white_logo.png'))
+                      ],
+                    ),
+                  ],
+                ),),
+                ContentsListSection(title: "픽사 오리지널", images: pixarContents),
+                ContentsListSection(title: "스타워즈", images: starwarsContents),
+                ContentsListSection(title: "한국 드라마", images: koreanContents),
+              ],
+            ),
+          )
+        ),
+        backgroundColor: Colors.transparent,
+        bottomNavigationBar: const CustomBottomNavigationBar(),
+      ),
     );
   }
 }
